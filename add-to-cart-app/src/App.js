492348555c881f01066166c0b4c668ae338c30b2
Syed-Hashma-au52 <syedhashma777@gmail.com>
2023-08-04 13:14:06 +0530

    interview

---
+++ add-to-cart-app/src/App.js
@@ -0,0 +1,40 @@
+// src/App.js
+import React, { useState } from 'react';
+import products from './data/products';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import './App.css';
+
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    const existingItem = cartItems.find(item => item.id === product.id);
+
+    if (existingItem) {
+      setCartItems(cartItems.map(item => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item));
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const handleUpdateQuantity = (productId, quantity) => {
+    setCartItems(cartItems.map(item => item.id === productId ? { ...item, quantity } : item));
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>My Online Store</h1>
+      </div>
+      <div className="product-list">
+        {products.map(product => (
+          <Product key={product.id} product={product} onAddToCart={handleAddToCart} />
+        ))}
+      </div>
+      <Cart cartItems={cartItems} onUpdateQuantity={handleUpdateQuantity} />
+    </div>
+  );
+};
+
+export default App;
---
+++ add-to-cart-app/src/components/Cart.js
@@ -0,0 +1,20 @@
+// src/components/Cart.js
+import React from 'react';
+
+const Cart = ({ cartItems }) => {
+  const totalPrice = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+  return (
+    <div className="cart">
+      <h2>Shopping Cart</h2>
+      {cartItems.map(item => (
+        <div key={item.id}>
+          <p>{item.name} - ${item.price} x {item.quantity}</p>
+        </div>
+      ))}
+      <h3>Total: ${totalPrice}</h3>
+    </div>
+  );
+};
+
+export default Cart;
